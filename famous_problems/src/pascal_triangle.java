f5ce61e0a1766f3de5099d6425b56a22c740a2c7
Corey Xue <coreyxueatcal@gmail.com>
2014-02-08 22:25:55 -0800

    add print mn

--- famous_problems/src/pascal_triangle.java
+++ famous_problems/src/pascal_triangle.java
@@ -1,5 +1,6 @@
 
 public class pascal_triangle {
+	//my version
 	public int[][] construc_triangle(int n)
 	{
 		int[][] pt = new int[n][];
@@ -17,11 +18,41 @@ public class pascal_triangle {
 		}
 		return pt;
 	}
+	//lecture version
+	public int[][] construct_triangle(int n)
+	{
+		int[][] pt = new int[n][];
+		for(int i=0;i<n;i++)
+		{
+			pt[i] = new int[i+1];
+			pt[i][0] = 1;
+			for(int j=1;j<i;j++)
+			{
+				pt[i][j] = pt[i-1][j-1]+pt[i-1][j];
+			}
+			pt[i][i] = 1;
+		}
+		return pt;
+	}
+	//get the element of mth row nth col,time O(n^2) space O(n)
 	public int get_mn(int m, int n)
 	{
 		int[] pre;
 		int[] cur;
-		
+		pre = new int[1];
+		pre[0]=1;
+		cur = new int[2];
+		cur[0]=1;cur[1]=1;
+		for(int i=3;i<=m;i++)
+		{
+			pre = cur;
+			cur = new int[i];
+			cur[0]=1;
+			for(int j=1;j<i-1;j++)
+				cur[j] = pre[j]+pre[j-1];
+			cur[i-1]=1;
+		}
+		return cur[n];
 		
 	}
 
--- famous_problems/src/pascal_triangleTest.java
+++ famous_problems/src/pascal_triangleTest.java
@@ -8,13 +8,14 @@ public class pascal_triangleTest {
 	@Test
 	public void test() {
 		pascal_triangle pt = new pascal_triangle();
-		int[][] result = pt.construc_triangle(5);
+		int[][] result = pt.construct_triangle(5);
 		for(int i=0;i<result.length;i++)
 		{
 			for(int j=0;j<result[i].length;j++)
 				System.out.print(result[i][j]);
 			System.out.print("\n");
 		}
+		System.out.print(pt.get_mn(5, 1));
 	}
 
 }
